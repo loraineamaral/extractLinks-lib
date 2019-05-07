b44e61324b88e430fe4cbd660f9324beebc1d60c
Loraine <loraineeamaral@gmail.com>
2019-05-07 14:24:49 -0300

    add readme

---
+++ test/extract-link.spec.js
@@ -0,0 +1,22 @@
+let chai = require("chai");
+let expect = chai.expect;
+let textString = require("../lib/index");
+
+describe("findURLsInText", () => {
+  
+  it("Testa se existem links no arquivo e retorna vazio", () => {
+    expect(textString.findURLsInText("`#Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).to.equal("[]")
+  });
+
+  it("Testa se existem links no arquivo e retorna 3 links", () => {
+    expect(textString.findURLsInText("`#Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et [dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. [foo](http://foo.com) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;")).to.deep.equal([ { href: 'https://en.wiktionary.org/wiki/labore',
+    text: 'labore' },
+  { href: 'https://en.wiktionary.org/wiki/dolore',
+    text: 'dolore' },
+  { href: 'http://foo.com', text: 'foo' } ])
+  });
+  it("Testa se existem links no arquivo e retorna 1 link", () => {
+    expect(textString.findURLsInText("`#Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut [labore](https://en.wiktionary.org/wiki/labore).`;")).to.deep.equal([ { href: 'https://en.wiktionary.org/wiki/labore',
+    text: 'labore' } ])
+});
+});
